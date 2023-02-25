24541c6acbb55ec7c2b069f3298c5bd9f871a4a7
XuzunogluX <88375162+XuzunogluX@users.noreply.github.com>
2023-02-25 17:29:48 +0300

    Create room.js

---
+++ room.js
@@ -0,0 +1,197 @@
+var map = `{"name":"RS | Ayı Boğan","width":1280,"height":640,"spawnDistance":512,"bg":{"type":"grass","width":1160,"height":578,"kickOffRadius":160,"cornerRadius":0},"ballPhysics":{"bCoef":0.5,"damping":0.989,"invMass":0.8,"radius":8},"playerPhysics":{"acceleration":0.12,"bCoef":0.2,"damping":0.96,"invMass":0.3,"kickStrength":8,"kickingAcceleration":0.07,"kickingDamping":0.96},"traits":{"ballArea":{"bCoef":0,"cMask":["ball"],"vis":false},"gameArea":{"bCoef":0,"cMask":["all"]},"line":{"cMask":[],"color":"c7e6bd","vis":true},"support":{"cMask":[],"color":"ffffff","vis":true,"radius":3},"goalNet":{"bCoef":0.6,"cMask":["all"],"color":"ffffff","vis":true},"goalPost":{"bCoef":1.4,"invMass":0,"radius":5},"kickOffBarrier":{"bCoef":0.1,"cMask":["red","blue"],"cGroup":["redKO","blueKO"],"vis":false},"corner":{"bCoef":-3.75,"cMask":["ball"],"color":"576c46","vis":true},"punt":{"bCoef":-5.63,"cMask":["ball"],"color":"576c46"}},"vertexes":[/*0*/{"x":-1160,"y":-549.2,"trait":"line"},/*1*/{"x":-1131.2,"y":-578,"trait":"line"},/*2*/{"x":-1160,"y":549.2,"trait":"line"},/*3*/{"x":-1131.2,"y":578,"trait":"line"},/*4*/{"x":-1160,"y":-336,"trait":"line"},/*5*/{"x":-872,"y":-336,"trait":"line","cMask":["c2"]},/*6*/{"x":-872,"y":336,"trait":"line","cMask":["c2"]},/*7*/{"x":-1160,"y":336,"trait":"line"},/*8*/{"x":-872,"y":-128,"trait":"line"},/*9*/{"x":-872,"y":128,"trait":"line"},/*10*/{"x":-968,"y":-3,"trait":"line"},/*11*/{"x":-968,"y":3,"trait":"line"},/*12*/{"x":-1160,"y":-192,"trait":"line"},/*13*/{"x":-1064,"y":-192,"trait":"line"},/*14*/{"x":-1064,"y":192,"trait":"line"},/*15*/{"x":-1160,"y":192,"trait":"line"},/*16*/{"x":-1160,"y":-112,"trait":"goalNet"},/*17*/{"x":-1160,"y":112,"trait":"goalNet"},/*18*/{"x":-1214,"y":-111,"trait":"goalNet"},/*19*/{"x":-1214,"y":111,"trait":"goalNet"},/*20*/{"x":-1234,"y":-131,"trait":"support"},/*21*/{"x":-1234,"y":131,"trait":"support"},/*22*/{"x":1160,"y":-549.2,"trait":"line"},/*23*/{"x":1131.2,"y":-578,"trait":"line"},/*24*/{"x":1160,"y":549.2,"trait":"line"},/*25*/{"x":1131.2,"y":578,"trait":"line"},/*26*/{"x":1160,"y":-336,"trait":"line"},/*27*/{"x":872,"y":-336,"trait":"line","cMask":["c2"]},/*28*/{"x":872,"y":336,"trait":"line","cMask":["c2"]},/*29*/{"x":1160,"y":336,"trait":"line"},/*30*/{"x":872,"y":-128,"trait":"line"},/*31*/{"x":872,"y":128,"trait":"line"},/*32*/{"x":968,"y":-3,"trait":"line"},/*33*/{"x":968,"y":3,"trait":"line"},/*34*/{"x":1160,"y":-192,"trait":"line"},/*35*/{"x":1064,"y":-192,"trait":"line"},/*36*/{"x":1064,"y":192,"trait":"line"},/*37*/{"x":1160,"y":192,"trait":"line"},/*38*/{"x":1160,"y":-112,"trait":"goalNet"},/*39*/{"x":1160,"y":112,"trait":"goalNet"},/*40*/{"x":1214,"y":-111,"trait":"goalNet"},/*41*/{"x":1214,"y":111,"trait":"goalNet"},/*42*/{"x":1234,"y":-131,"trait":"support"},/*43*/{"x":1234,"y":131,"trait":"support"},/*44*/{"x":-1184,"y":-232,"trait":"ballArea"},/*45*/{"x":-1184,"y":-136,"trait":"ballArea"},/*46*/{"x":-1184,"y":136,"trait":"ballArea"},/*47*/{"x":-1184,"y":232,"trait":"ballArea"},/*48*/{"x":-1192,"y":-232,"trait":"ballArea"},/*49*/{"x":-1192,"y":-136,"trait":"ballArea"},/*50*/{"x":-1192,"y":136,"trait":"ballArea"},/*51*/{"x":-1192,"y":232,"trait":"ballArea"},/*52*/{"x":1184,"y":-232,"trait":"ballArea"},/*53*/{"x":1184,"y":-136,"trait":"ballArea"},/*54*/{"x":1184,"y":136,"trait":"ballArea"},/*55*/{"x":1184,"y":232,"trait":"ballArea"},/*56*/{"x":1192,"y":-232,"trait":"ballArea"},/*57*/{"x":1192,"y":-136,"trait":"ballArea"},/*58*/{"x":1192,"y":136,"trait":"ballArea"},/*59*/{"x":1192,"y":232,"trait":"ballArea"},/*60*/{"x":0,"y":-640,"trait":"kickOffBarrier"},/*61*/{"x":0,"y":-160,"trait":"kickOffBarrier"},/*62*/{"x":0,"y":160,"trait":"kickOffBarrier"},/*63*/{"x":0,"y":640,"trait":"kickOffBarrier"},/*64*/{"x":0,"y":-5,"trait":"line"},/*65*/{"x":0,"y":5,"trait":"line"},/*66*/{"x":-1160,"y":-514,"trait":"line"},/*67*/{"x":1160,"y":-514,"trait":"line"},/*68*/{"x":-1160,"y":514,"trait":"line"},/*69*/{"x":1160,"y":514,"trait":"line"},/*70*/{"x":-1160,"y":-242,"trait":"line","cMask":["c1"]},/*71*/{"x":-824,"y":-578,"trait":"line","cMask":["c1"]},/*72*/{"x":-1160,"y":242,"trait":"line","cMask":["c1"]},/*73*/{"x":-824,"y":578,"trait":"line","cMask":["c1"]},/*74*/{"x":1160,"y":-242,"trait":"line","cMask":["c1"]},/*75*/{"x":824,"y":-578,"trait":"line","cMask":["c1"]},/*76*/{"x":1160,"y":242,"trait":"line","cMask":["c1"]},/*77*/{"x":824,"y":578,"trait":"line","cMask":["c1"]},/*78*/{"x":-1206,"y":-562,"trait":"corner"},/*79*/{"x":-1176,"y":-578,"trait":"corner"},/*80*/{"x":-1206,"y":562,"trait":"corner"},/*81*/{"x":-1176,"y":578,"trait":"corner"},/*82*/{"x":1206,"y":-562,"trait":"corner"},/*83*/{"x":1176,"y":-578,"trait":"corner"},/*84*/{"x":1206,"y":562,"trait":"corner"},/*85*/{"x":1176,"y":578,"trait":"corner"},/*86*/{"x":-1280,"y":-514,"vis":false,"cMask":["c0"]},/*87*/{"x":1280,"y":-514,"vis":false,"cMask":["c0"]},/*88*/{"x":1280,"y":514,"cMask":["c0"]},/*89*/{"x":-1280,"y":514,"cMask":["c0"]},/*90*/{"x":-824,"y":-640,"cMask":["c1"]},/*91*/{"x":-1280,"y":-242,"cMask":["c1"]},/*92*/{"x":-1280,"y":242,"cMask":["c1"]},/*93*/{"x":-824,"y":640,"cMask":["c1"]},/*94*/{"x":824,"y":640,"cMask":["c1"]},/*95*/{"x":1280,"y":242,"cMask":["c1"]},/*96*/{"x":1280,"y":-242,"cMask":["c1"]},/*97*/{"x":824,"y":-640,"cMask":["c1"]},/*98*/{"x":-1280,"y":-336,"cMask":["c2"]},/*99*/{"x":-1280,"y":336,"cMask":["c2"]},/*100*/{"x":1280,"y":336,"cMask":["c2"]},/*101*/{"x":1280,"y":-336,"cMask":["c2"]}],"segments":[{"v0":0,"v1":1,"curve":-90,"trait":"line"},{"v0":2,"v1":3,"curve":90,"trait":"line"},{"v0":4,"v1":5,"trait":"line"},{"v0":5,"v1":6,"trait":"line","cMask":["c2"]},{"v0":6,"v1":7,"trait":"line"},{"v0":8,"v1":9,"curve":105,"trait":"line"},{"v0":10,"v1":11,"trait":"line"},{"v0":10,"v1":11,"curve":-180,"trait":"line"},{"v0":10,"v1":11,"curve":180,"trait":"line"},{"v0":12,"v1":13,"trait":"line"},{"v0":13,"v1":14,"trait":"line"},{"v0":14,"v1":15,"trait":"line"},{"v0":16,"v1":18,"curve":-5,"trait":"goalNet"},{"v0":18,"v1":19,"curve":-10,"trait":"goalNet"},{"v0":19,"v1":17,"curve":-5,"trait":"goalNet"},{"v0":18,"v1":20,"cMask":[],"trait":"goalNet"},{"v0":19,"v1":21,"cMask":[],"trait":"goalNet"},{"v0":22,"v1":23,"curve":90,"trait":"line"},{"v0":24,"v1":25,"curve":-90,"trait":"line"},{"v0":26,"v1":27,"trait":"line"},{"v0":27,"v1":28,"trait":"line","cMask":["c2"]},{"v0":28,"v1":29,"trait":"line"},{"v0":30,"v1":31,"curve":-105,"trait":"line"},{"v0":32,"v1":33,"trait":"line"},{"v0":32,"v1":33,"curve":-180,"trait":"line"},{"v0":32,"v1":33,"curve":180,"trait":"line"},{"v0":34,"v1":35,"trait":"line"},{"v0":35,"v1":36,"trait":"line"},{"v0":36,"v1":37,"trait":"line"},{"v0":38,"v1":40,"curve":5,"trait":"goalNet"},{"v0":40,"v1":41,"curve":10,"trait":"goalNet"},{"v0":41,"v1":39,"curve":5,"trait":"goalNet"},{"v0":40,"v1":42,"cMask":[],"trait":"goalNet"},{"v0":41,"v1":43,"cMask":[],"trait":"goalNet"},{"v0":44,"v1":45,"curve":30,"trait":"punt"},{"v0":46,"v1":47,"curve":30,"trait":"punt"},{"v0":44,"v1":48,"vis":true,"trait":"ballArea"},{"v0":45,"v1":49,"vis":true,"trait":"ballArea"},{"v0":46,"v1":50,"vis":true,"trait":"ballArea"},{"v0":47,"v1":51,"vis":true,"trait":"ballArea"},{"v0":52,"v1":53,"curve":-30,"trait":"punt"},{"v0":54,"v1":55,"curve":-30,"trait":"punt"},{"v0":52,"v1":56,"vis":true,"trait":"ballArea"},{"v0":53,"v1":57,"vis":true,"trait":"ballArea"},{"v0":54,"v1":58,"vis":true,"trait":"ballArea"},{"v0":55,"v1":59,"vis":true,"trait":"ballArea"},{"v0":60,"v1":61,"trait":"kickOffBarrier"},{"v0":61,"v1":62,"curve":180,"cGroup":["redKO"],"trait":"kickOffBarrier"},{"v0":61,"v1":62,"curve":-180,"cGroup":["blueKO"],"trait":"kickOffBarrier"},{"v0":62,"v1":63,"trait":"kickOffBarrier"},{"v0":64,"v1":65,"trait":"line"},{"v0":64,"v1":65,"curve":180,"trait":"line"},{"v0":64,"v1":65,"curve":-180,"trait":"line"},{"v0":64,"v1":65,"curve":120,"trait":"line"},{"v0":64,"v1":65,"curve":-120,"trait":"line"},{"v0":66,"v1":67,"color":"5e844d","trait":"line"},{"v0":68,"v1":69,"color":"5e844d","trait":"line"},{"v0":70,"v1":71,"curve":-90,"color":"5e844d","cMask":["c1"]},{"v0":72,"v1":73,"curve":90,"color":"5e844d","cMask":["c1"]},{"v0":74,"v1":75,"curve":90,"color":"5e844d","cMask":["c1"]},{"v0":76,"v1":77,"curve":-90,"color":"5e844d","cMask":["c1"]},{"v0":78,"v1":79,"curve":-60,"trait":"corner"},{"v0":80,"v1":81,"curve":60,"trait":"corner"},{"v0":82,"v1":83,"curve":60,"trait":"corner"},{"v0":84,"v1":85,"curve":-60,"trait":"corner"},{"vis":false,"v0":86,"v1":87,"cMask":["c0"]},{"vis":false,"v0":88,"v1":89,"cMask":["c0"]},{"vis":false,"v0":71,"v1":90,"cMask":["c1"]},{"vis":false,"v0":70,"v1":91,"cMask":["c1"]},{"vis":false,"v0":72,"v1":92,"cMask":["c1"]},{"vis":false,"v0":73,"v1":93,"cMask":["c1"]},{"vis":false,"v0":77,"v1":94,"cMask":["c1"]},{"vis":false,"v0":76,"v1":95,"cMask":["c1"]},{"vis":false,"v0":74,"v1":96,"cMask":["c1"]},{"vis":false,"v0":75,"v1":97,"cMask":["c1"]},{"vis":false,"v0":5,"v1":98,"cMask":["c2"]},{"vis":false,"v0":6,"v1":99,"cMask":["c2"]},{"vis":false,"v0":28,"v1":100,"cMask":["c2"]},{"vis":false,"v0":27,"v1":101,"cMask":["c2"]}],"discs":[{"pos":[-1160,-578],"trait":"support"},{"pos":[-1160,578],"trait":"support"},{"pos":[1160,-578],"trait":"support"},{"pos":[1160,578],"trait":"support"},{"pos":[-1234,-131],"trait":"support"},{"pos":[-1234,131],"trait":"support"},{"pos":[1234,-131],"trait":"support"},{"pos":[1234,131],"trait":"support"},{"pos":[-1160,-112],"trait":"goalPost"},{"pos":[-1160,112],"trait":"goalPost"},{"pos":[1160,-112],"trait":"goalPost"},{"pos":[1160,112],"trait":"goalPost"}],"goals":[{"p0":[-1168,-112],"p1":[-1168,112],"team":"red"},{"p0":[1168,-112],"p1":[1168,112],"team":"blue"}],"planes":[{"normal":[1,0],"dist":-1280,"trait":"gameArea"},{"normal":[-1,0],"dist":-1280,"trait":"gameArea"},{"normal":[0,1],"dist":-640,"trait":"gameArea"},{"normal":[0,-1],"dist":-640,"trait":"gameArea"},{"normal":[1,0],"dist":-1222,"trait":"ballArea"},{"normal":[-1,0],"dist":-1222,"trait":"ballArea"},{"normal":[0,1],"dist":-610,"trait":"ballArea"},{"normal":[0,-1],"dist":-610,"trait":"ballArea"}]}`;
+
+var room = HBInit({
+  roomName: "Real Soccer | Kurallı",
+  maxPlayers: 12,
+  noPlayer: true,
+  public: false,
+  geo : {"code": "tr", "lat" : 41.01384, "lon" : 28.94966}
+});
+
+room.setTeamsLock(true);
+room.setCustomStadium(map);
+
+const locations = {INGAME:0, THROWIN:1, CORNER:2, GOALKICK:3};
+const radiusPlayer = 15;
+const radiusBall = 8;
+var triggerDistance = radiusPlayer + radiusBall + 0.1;
+var lastPlayerTouched = ["",""];
+var lastPlayerKicked = "";
+var ballLocation;
+var admins = [
+  "coYrF6OewCrkE37E72hau7YyPnT2actdjmjuTk5TGo8",
+  "M0DABdSNd6Y-A6BlZiorLJAUOfWb1i7OqHLb7jr1rQY"
+];
+var blackList = [];
+var commands = ["!adminçağır"];
+
+room.onPlayerJoin = function(player) {
+  if(admins.includes(player.auth)) {
+    room.setPlayerAdmin(player.id, true);
+    room.sendAnnouncement("🐻 DESTUUUR! SULTAN AYI BOĞAN HAN HAZRETLERİ 🐻", null, 0xf4d03f, "bold", 2);
+  }
+  if(blackList.includes(player.auth)) {
+  	room.kickPlayer(player.id, "Süresiz banlandın!", true);
+  }
+  room.sendAnnouncement(`Hoş geldin ${player.name}`, null, 0x00ff00, null, 2);
+}
+
+room.onPlayerChat = function(player, message) {
+	if(message == "!") {
+  	var discProps = room.getPlayerDiscProperties(player.id);
+  	room.sendAnnouncement(`Taç: ${(discProps.cGroup & room.CollisionFlags.c0) != 0}`);
+    room.sendAnnouncement(`Korner: ${(discProps.cGroup & room.CollisionFlags.c1) != 0}`);
+    room.sendAnnouncement(`Aut: ${(discProps.cGroup & room.CollisionFlags.c2) != 0}`);
+  	return false;
+  }
+  else if (player.admin) {
+   	room.sendAnnouncement(`👑 ${player.name}: ${message}`, null, 0xffd700, "bold", 1);
+    return false;
+  }
+  else {
+    room.sendAnnouncement(`${player.name}: ${message.toLowerCase()}`, null, null, null, 1);
+    return false;
+  }
+}
+
+room.onPlayerBallKick = function(player) {
+	lastPlayerKicked = player;
+}
+
+room.onTeamGoal = function(team) {
+	if(team == lastPlayerTouched[0].team) {
+  	if(lastPlayerTouched[0].team == lastPlayerTouched[1].team) {
+    	room.sendAnnouncement(`⚽ GOOOL! ${lastPlayerTouched[0].name} Asist: ${lastPlayerTouched[1].name} ⚽`, null, 0x00ffff, null, 2);
+    }
+    else {
+    	room.sendAnnouncement(`⚽ GOOOL! ${lastPlayerTouched[0].name} ⚽`, null, 0x00ffff, null, 2);
+    }
+  }
+	else {
+  	if(team != lastPlayerKicked.team) {
+    	room.sendAnnouncement(`😢 GOOOL! ${lastPlayerKicked.name} kendi kalesine 😢`, null, 0x00ffff, null, 2);
+    }
+    else {
+    	room.sendAnnouncement(`⚽ GOOOL! ${lastPlayerTouched[1].name} ⚽`, null, 0x00ffff, null, 2);
+    }
+  }
+}
+
+function getLastPlayerTouched() { // Topa son dokunan oyuncuyu belirler
+	var ballPosition = room.getBallPosition();
+  var players = room.getPlayerList();
+  for(var i=0; i<players.length; i++) {
+  	if(players[i].position != null) {
+    	var distanceToBall = pointDistance(ballPosition,players[i].position);
+      if(distanceToBall < triggerDistance) {
+      	if(lastPlayerTouched[0].id != players[i].id) {
+        	lastPlayerTouched[1] = lastPlayerTouched[0];
+        	lastPlayerTouched[0] = players[i];
+        }
+      }
+    }
+  }
+}
+
+function pointDistance(p1,p2){ // İki nokta arasındaki uzaklığı hesaplar
+    var d1 = p1.x-p2.x;
+    var d2 = p1.y-p2.y;
+    return Math.sqrt(d1*d1 + d2*d2);
+}
+
+function checkBallPosition() { // Taç, Korner, Aut durumlarında çalışır
+	var bp = room.getBallPosition();
+  if(Math.abs(bp.x) < 1168 && Math.abs(bp.y) < 586) {
+  	resetFlags();
+  	ballLocation = locations.INGAME;
+  }
+  else if(ballLocation == locations.INGAME) {
+  	if(Math.abs(bp.y) > 586) {
+      throwIn(bp);
+    }
+    else if(Math.abs(bp.x) > 1168 && Math.abs(bp.y) > 112 && Math.sign(bp.x) == lastPlayerTouched[0].team*2-3) {
+    	corner(bp);
+    }
+    else if(Math.abs(bp.x) > 1168 && Math.abs(bp.y) > 112 && Math.sign(bp.x) != lastPlayerTouched[0].team*2-3){
+    	goalKick(bp);
+    }
+  	ballInGame = false;
+  }
+}
+
+function throwIn(bp) { // Taç atışını yönetir
+	var players = room.getPlayerList();
+  room.setDiscProperties(0, {y: Math.sign(bp.y)*591, xspeed: 0, yspeed: 0});
+  ballLocation = locations.THROWIN;
+  if(lastPlayerTouched[0].team == 1) {room.sendAnnouncement("[🔵] Taç mavi takımda")}
+  else{room.sendAnnouncement("[🔴] Taç kırmızı takımda")}
+  for(var i=0; i<players.length; i++) {
+  	if(players[i].team == lastPlayerTouched[0].team) {
+    	var discProps = room.getPlayerDiscProperties(players[i].id);
+    	room.setPlayerDiscProperties(players[i].id, {cGroup: discProps.cGroup | room.CollisionFlags.c0});
+      if(Math.sign(players[i].position.y) == Math.sign(bp.y) && Math.abs(players[i].position.y) > 499) {
+      	room.setPlayerDiscProperties(players[i].id, {y: Math.sign(players[i].position.y)*499});
+      }
+    }
+  }
+}
+
+function corner(bp) { // Korner vuruşlarını yönetir
+	var players = room.getPlayerList();
+  room.setDiscProperties(0, {x: Math.sign(bp.x)*1200, y: Math.sign(bp.y)*590, xspeed: 0, yspeed: 0});
+  room.sendAnnouncement("[🚩] Köşe vuruşu");
+  ballLocation = locations.CORNER;
+  for(var i=0; i<players.length; i++) {
+    if(players[i].team == lastPlayerTouched[0].team) {
+      var discProps = room.getPlayerDiscProperties(players[i].id);
+      room.setPlayerDiscProperties(players[i].id, {cGroup: discProps.cGroup | room.CollisionFlags.c1});
+      bp = room.getBallPosition();
+      if(pointDistance(bp,players[i].position) < 390) {
+        room.setPlayerDiscProperties(players[i].id, {y: Math.sign(bp.y)*227}); 
+      }
+    }
+  }
+}
+
+function goalKick(bp) { // Kale vuruşlarını yönetir
+	var players = room.getPlayerList();
+	room.setDiscProperties(0, {x: Math.sign(bp.x)*1210, y: Math.sign(bp.y)*184, xspeed: 0, yspeed: 0});
+  room.sendAnnouncement("[🥅] Kale vuruşu");
+  ballLocation = locations.GOALKICK;
+  for(var i=0; i<players.length; i++) {
+   	if(players[i].team == lastPlayerTouched[0].team) {
+     	var discProps = room.getPlayerDiscProperties(players[i].id);
+     	room.setPlayerDiscProperties(players[i].id, {cGroup: discProps.cGroup | room.CollisionFlags.c2});
+      if(Math.sign(players[i].position.x) == Math.sign(bp.x) && Math.abs(players[i].position.x) > 857) {
+     	  room.setPlayerDiscProperties(players[i].id, {x: Math.sign(bp.x)*857});
+      }
+    }
+  }
+}
+
+function resetFlags() { // Flag'ları sıfırlar
+	var players = room.getPlayerList();
+  for(var i=0; i<players.length; i++) {
+  	if(players[i].team == 1) {
+    	room.setPlayerDiscProperties(players[i].id, {cGroup: room.CollisionFlags.red});
+    }
+    else if(players[i].team == 2) {
+    	room.setPlayerDiscProperties(players[i].id, {cGroup: room.CollisionFlags.blue});
+    }
+  }
+}
+
+room.onPlayerTeamChange = function(changedPlayer, byPlayer) {
+	if(changedPlayer.team == lastPlayerTouched[0].team) {
+  	var cf = room.CollisionFlags;
+  	var discProps = room.getPlayerDiscProperties(changedPlayer.id);
+  	if(ballLocation == locations.THROWIN) {room.setPlayerDiscProperties(changedPlayer.id, {cGroup: discProps.cGroup | cf.c0});}
+    else if(ballLocation == locations.CORNER) {room.setPlayerDiscProperties(changedPlayer.id, {cGroup: discProps.cGroup | cf.c1});}
+    else if(ballLocation == locations.GOALKICK) {room.setPlayerDiscProperties(changedPlayer.id, {cGroup: discProps.cGroup | cf.c2});}
+  }
+}
+
+room.onGameTick = function() {
+	getLastPlayerTouched();
+  checkBallPosition();
+}
